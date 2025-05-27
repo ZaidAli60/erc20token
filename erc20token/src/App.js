e4194e869253bb3f1184d5fa0f787432d0d8ee5f
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2025-05-27 05:07:28 +0000

    write a smart contract for custom token and deploy

--- erc20token/src/App.js
+++ erc20token/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>EmergingTectGrid</h1>
+      <h1 className='text-primary'>EmergingTectGrid</h1>
     </div>
   );
 }
